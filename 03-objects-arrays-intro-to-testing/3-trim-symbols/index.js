db3d18270b791b935755dd5725de0199ae768472
Kuchurov <kuchurov@imac-kucurov.local>
2022-09-27 12:52:25 +0000

    fix(task 3): trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,6 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-  let regexp = new RegExp('(?=(.))\\1{' + size + ',}', 'g')
-  return string.replace(regexp, (m, a) => a.repeat(size))
+  if (size < 1) return ''
+  if (!size) return string
+
+  const result = []
+  let count = 0
+
+  for (const char of string) {
+    if (result.at(-1) === char) {
+      if (count < size) {
+        result.push(char)
+        count++
+      }
+    } else {
+      count = 1
+      result.push(char)
+    }
+  }
+
+  return result.join('')
+
+  // let regexp = new RegExp('(?=(.))\\1{' + size + ',}', 'g')
+  // return string.replace(regexp, (m, a) => a.repeat(size))
 }
