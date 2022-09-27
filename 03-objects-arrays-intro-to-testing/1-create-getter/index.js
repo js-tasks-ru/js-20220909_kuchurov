3d11d6274560e420b985b66a38f7f3307648e93a
Kuchurov <kuchurov@imac-kucurov.local>
2022-09-27 12:52:12 +0000

    fix(task 3): createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,13 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-  path = path?.split('.') || path
+  const pathArr = path?.split('.')
   return function(obj) {
     let result = obj
-    for (let key of path) {
+
+    for (let key of pathArr) {
       if (!result) break
       result = result[key]
     }
+
     return result
   }
 }
